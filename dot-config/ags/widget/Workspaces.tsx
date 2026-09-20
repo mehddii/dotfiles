7737832771af42a1faa3d2c1c7bdf4dc6b32b4f2
Mahdi Bahous <mehdi7bs@gmail.com>
2026-09-20 21:55:54 +0000

    feat: Handling workspace with focus

--- dot-config/ags/widget/Workspaces.tsx
+++ dot-config/ags/widget/Workspaces.tsx
@@ -4,20 +4,31 @@ import { Gtk } from "ags/gtk4";
 import { For, Accessor, createBinding } from "ags";
 import { execAsync } from "ags/process";
 
+const hyprland = Hyprland.get_default()
+
 function Workspace({ id }: { id: number }): GObject.Object {
     const handleFocus = () => {
         execAsync(`hyprctl dispatch workspace ${id}`)
     }
 
+    const cssClass = createBinding(hyprland, "focused_workspace")
+        .as((focused) => {
+            const focusedId = focused.get_id()
+            return focusedId === id ? "active-btn" : ""
+        })
+
     return (
-        <button label={`${id}`} onClicked={handleFocus}>
+        <button
+            label={`${id}`}
+            onClicked={handleFocus}
+            class={cssClass}
+        >
         </button>
     )
 }
 
 // #amount workspaces shown by default, the rest are added and removed dynamically
 export function Workspaces({ amount }: { amount: number }): GObject.Object {
-    const hyprland = Hyprland.get_default()
     const workspaces = createBinding(hyprland, "workspaces")
         .as(
             // Merge the active workspaces with the defaults one.
